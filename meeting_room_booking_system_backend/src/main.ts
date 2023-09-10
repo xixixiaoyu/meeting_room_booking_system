4a1abfa5b57c74f828ebbaad6428046bd26167ec
yunumu <1416900346@qq.com>
2023-09-10 20:37:14 +0800

    用户登录下发token

--- meeting_room_booking_system_backend/src/main.ts
+++ meeting_room_booking_system_backend/src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+// import { FormatResponseInterceptor } from './format-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalInterceptors(new FormatResponseInterceptor());
 
   const configService = app.get(ConfigService);
 
