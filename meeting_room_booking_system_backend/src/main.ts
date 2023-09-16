f1facf64aad95b849fe7b4ae0c107d1245b7ef03
yunumu <1416900346@qq.com>
2023-09-16 13:39:10 +0800

    增加拦截器，修改接口格式和打印日志

--- meeting_room_booking_system_backend/src/main.ts
+++ meeting_room_booking_system_backend/src/main.ts
@@ -2,13 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-// import { FormatResponseInterceptor } from './format-response.interceptor';
+import { FormatResponseInterceptor } from './format-response.interceptor';
+import { InvokeRecordInterceptor } from './invoke-record.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  // app.useGlobalInterceptors(new FormatResponseInterceptor());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
+  app.useGlobalInterceptors(new InvokeRecordInterceptor());
 
   const configService = app.get(ConfigService);
 
