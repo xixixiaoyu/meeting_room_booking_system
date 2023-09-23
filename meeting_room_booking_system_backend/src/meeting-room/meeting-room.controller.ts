5ee38cb393b01156e6785e3d0e0ad44404effae2
yunumu <1416900346@qq.com>
2023-09-23 15:40:19 +0800

    初始化数据库数据

---
+++ meeting_room_booking_system_backend/src/meeting-room/meeting-room.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  DefaultValuePipe,
+} from '@nestjs/common';
+import { MeetingRoomService } from './meeting-room.service';
+import { generateParseIntPipe } from 'src/utils';
+
+@Controller('meeting-room')
+export class MeetingRoomController {
+  constructor(private readonly meetingRoomService: MeetingRoomService) {}
+
+  @Get('list')
+  async list(
+    @Query('pageNo', new DefaultValuePipe(1), generateParseIntPipe('pageNo'))
+    pageNo: number,
+    @Query(
+      'pageSize',
+      new DefaultValuePipe(2),
+      generateParseIntPipe('pageSize'),
+    )
+    pageSize: number,
+  ) {
+    return await this.meetingRoomService.find(pageNo, pageSize);
+  }
+}
---
+++ meeting_room_booking_system_backend/src/meeting-room/meeting-room.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateMeetingRoomDto } from './dto/create-meeting-room.dto';
+import { UpdateMeetingRoomDto } from './dto/update-meeting-room.dto';
+import { MeetingRoom } from './entities/meeting-room.entity';
+
+@Injectable()
+export class MeetingRoomService {
+  @InjectRepository(MeetingRoom)
+  private repository: Repository<MeetingRoom>;
+
+  initData() {
+    const room1 = new MeetingRoom();
+    room1.name = '木星';
+    room1.capacity = 10;
+    room1.equipment = '白板';
+    room1.location = '一层西';
+
+    const room2 = new MeetingRoom();
+    room2.name = '金星';
+    room2.capacity = 5;
+    room2.equipment = '';
+    room2.location = '二层东';
+
+    const room3 = new MeetingRoom();
+    room3.name = '天王星';
+    room3.capacity = 30;
+    room3.equipment = '白板，电视';
+    room3.location = '三层东';
+
+    this.repository.insert([room1, room2, room3]);
+  }
+}
